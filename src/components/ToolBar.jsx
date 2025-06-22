c7236b062491aae106315a5884cee934b18bbea4
Pascal Horton <pascal.horton@terranum.ch>
2025-06-22 23:22:58 +0200

    Adding time navigation icons.

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -4,16 +4,22 @@ import FrameDistributionsIcon from '../assets/toolbar/frame_distributions.svg?re
 import FrameAnalogsIcon from '../assets/toolbar/frame_analogs.svg?react';
 import PreferencesIcon from '../assets/toolbar/preferences.svg?react';
 
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+
+
 function ToolbarSquares() {
     const squares = [
-        { color: '#FFD700', date: '15.06' },
-        { color: '#FFC300', date: '16.06' },
-        { color: '#FFB000', date: '17.06' },
-        { color: '#FF9500', date: '18.06' },
-        { color: '#FF7F00', date: '19.06' },
-        { color: '#FF5E13', date: '20.06' },
-        { color: '#FF3B1F', date: '21.06' },
-        { color: '#FF1A1A', date: '22.06' },
+        {color: '#FFD700', date: '15.06'},
+        {color: '#FFC300', date: '16.06'},
+        {color: '#FFB000', date: '17.06'},
+        {color: '#FF9500', date: '18.06'},
+        {color: '#FF7F00', date: '19.06'},
+        {color: '#FF5E13', date: '20.06'},
+        {color: '#FF3B1F', date: '21.06'},
+        {color: '#FF1A1A', date: '22.06'},
     ];
     return (
         <div className="toolbar-left">
@@ -21,7 +27,7 @@ function ToolbarSquares() {
                 <div
                     key={i}
                     className="toolbar-square"
-                    style={{ background: sq.color }}
+                    style={{background: sq.color}}
                 >
                     <span>{sq.date}</span>
                 </div>
@@ -33,7 +39,13 @@ function ToolbarSquares() {
 function ToolbarCenter() {
     return (
         <div className="toolbar-center">
-            <div>Prévision du 15.06.2025 00h</div>
+            <div className="toolbar-center-row">
+                <span>Prévision du 15.06.2025 00h</span>
+                <button className="toolbar-center-btn"><KeyboardDoubleArrowLeftIcon fontSize="small" /></button>
+                <button className="toolbar-center-btn"><KeyboardArrowLeftIcon fontSize="small" /></button>
+                <button className="toolbar-center-btn"><KeyboardArrowRightIcon fontSize="small" /></button>
+                <button className="toolbar-center-btn"><KeyboardDoubleArrowRightIcon fontSize="small" /></button>
+            </div>
             <div>Analogie circulation (4Zo) ARPEGE (4Zo-ARPGEPE)</div>
         </div>
     );
@@ -42,12 +54,12 @@ function ToolbarCenter() {
 export default function ToolBar() {
     return (
         <header className="toolbar">
-            <ToolbarSquares />
-            <ToolbarCenter />
+            <ToolbarSquares/>
+            <ToolbarCenter/>
             <div className="toolbar-right">
-                <button className="toolbar-icon-btn"><FrameDistributionsIcon /></button>
-                <button className="toolbar-icon-btn"><FrameAnalogsIcon /></button>
-                <button className="toolbar-icon-btn"><PreferencesIcon /></button>
+                <button className="toolbar-icon-btn"><FrameDistributionsIcon/></button>
+                <button className="toolbar-icon-btn"><FrameAnalogsIcon/></button>
+                <button className="toolbar-icon-btn"><PreferencesIcon/></button>
             </div>
         </header>
     );
