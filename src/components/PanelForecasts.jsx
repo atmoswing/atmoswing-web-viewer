e4bf5e3ed33543ada436434a3ffcca754dad6df9
Pascal Horton <pascal.horton@terranum.ch>
2025-06-23 00:14:24 +0200

    Adding a TreeView for the forecasts.

--- src/components/PanelForecasts.jsx
+++ src/components/PanelForecasts.jsx
@@ -1,11 +1,22 @@
+import * as React from 'react';
 import Panel from './Panel';
+import {SimpleTreeView} from '@mui/x-tree-view/SimpleTreeView';
+import {TreeItem} from '@mui/x-tree-view/TreeItem';
+
 
 export default function PanelForecasts(props) {
     return (
         <Panel title="Forecasts" defaultOpen={props.defaultOpen}>
-            <div className="panel-content">
-                <p>....</p>
-            </div>
+            <SimpleTreeView>
+                <TreeItem itemId="1" label="Analogie circulation (4Zo) ARPEGE">
+                    <TreeItem itemId="2" label="Alpes du Nord" />
+                    <TreeItem itemId="3" label="Alpes du Sud" />
+                </TreeItem>
+                <TreeItem itemId="4" label="Analogie circulation (4Zo) CEP">
+                    <TreeItem itemId="5" label="Alpes du Nord" />
+                    <TreeItem itemId="6" label="Alpes du Sud" />
+                </TreeItem>
+            </SimpleTreeView>
         </Panel>
     );
 }
