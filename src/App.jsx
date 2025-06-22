fd9bd33b8ee5aa1ee9860a75e5d05f9b08d41f89
Pascal Horton <pascal.horton@terranum.ch>
2025-06-22 22:11:43 +0200

    Removing unused icons.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import './styles/App.css'
 
 import MapViewer from "./components/MapViewer.jsx";
 
-import MapSelectIcon from './assets/toolbar/map_select.svg?react';
-import MapZoomInIcon from './assets/toolbar/map_zoom_in.svg?react';
-import MapZoomOutIcon from './assets/toolbar/map_zoom_out.svg?react';
-import MapMoveIcon from './assets/toolbar/map_move.svg?react';
-import MapFitIcon from './assets/toolbar/map_fit.svg?react';
 import FrameDistributionsIcon from './assets/toolbar/frame_distributions.svg?react';
 import FrameAnalogsIcon from './assets/toolbar/frame_analogs.svg?react';
 import PreferencesIcon from './assets/toolbar/preferences.svg?react';
@@ -52,11 +47,6 @@ function SideBar() {
 function ToolBar() {
     return (
         <header className="toolbar">
-            <button className="toolbar-icon-btn"><MapSelectIcon /></button>
-            <button className="toolbar-icon-btn"><MapZoomInIcon /></button>
-            <button className="toolbar-icon-btn"><MapZoomOutIcon /></button>
-            <button className="toolbar-icon-btn"><MapMoveIcon /></button>
-            <button className="toolbar-icon-btn"><MapFitIcon /></button>
             <button className="toolbar-icon-btn"><FrameDistributionsIcon /></button>
             <button className="toolbar-icon-btn"><FrameAnalogsIcon /></button>
             <button className="toolbar-icon-btn"><PreferencesIcon /></button>
